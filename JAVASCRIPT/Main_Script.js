94ac741dcc5bca10efcda0d68c21ce0c14e81211
ROYY-Y <punkung159951@gmail.com>
2025-11-06 14:33:31 +0700

    Pun update all profile feature

---
+++ JAVASCRIPT/Main_Script.js
@@ -0,0 +1,9 @@
+const popUpMain = document.getElementById("profile-pop-up-main");
+
+function openPopup(){
+    popUpMain.style.display = "flex";
+}
+
+function closePopup(){
+    popUpMain.style.display = "none";
+}
