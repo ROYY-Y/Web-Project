7730ad79e608c262fa3ad3d7abb4739e21e6dd63
ROYY-Y <punkung159951@gmail.com>
2025-11-22 22:04:00 +0700

    Pun update backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors'); // Import CORS middleware
 const mysql = require('mysql2');
-require('dotenv').config(); // Load environment variables from .env file 
+require('dotenv').config({ path: __dirname + '/.env' });// Load environment variables from .env file 
+
 
 const port = process.env.PORT || 3000;
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
-
 
 const db = mysql.createConnection({ // mysql connection setup (using environment variables)
   host: process.env.DB_HOST,
@@ -27,11 +27,15 @@ db.connect((err) => { // Connect to the database
   console.log('Connected to the MySQL database.');
 });
 
+
+app.get('/collection', (req, res) => { // Collection route
+  res.send('Hello World!');
+});
+
+
+
+
 // Finish setting up and connection
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
